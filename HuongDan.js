038c2bd99e8b87fa5015dcdb669cf9953d555ff8
quynhosuperkid <hoangthibichvan95@gmail.com>
2017-04-03 03:31:18 +0700

    1412689

---
+++ HuongDan.js
@@ -0,0 +1,2 @@
+- Sử dụng git theo centralized workflow:  đơn thuần là để lấy code 
+- Sử dụng git theo feature branch workflow: là dùng để tạo nhánh (creale branch) --> Switch/Checkout thành tên nhánh vừa tạo
